e20f075b8c0ff856836f179578ff848ed773f708
Amberley Romo <amberleyjohanna@gmail.com>
2020-08-08 18:35:10 -0500

    fix the header

--- src/components/header.js
+++ src/components/header.js
@@ -1,5 +1,5 @@
 /** @jsx jsx */
-import { jsx, Flex, useColorMode } from "theme-ui"
+import { jsx, Flex, Box, useColorMode } from "theme-ui"
 import SkipLink from "./skip-link"
 import NavLink from "./nav-link"
 import Button from "./button"
@@ -16,36 +16,44 @@ export default () => {
   }
 
   return (
-    <Flex
+    <Box
       as="header"
       sx={{
-        justifyContent: "space-between",
-        maxWidth: 1024,
-        m: `auto`,
-        py: 3,
+        width: `100%`,
+        px: 3,
       }}
     >
-      <SkipLink>Skip to content</SkipLink>
-      <Flex>
-        <NavLink to="/">
-          A11y Coffee{" "}
-          <span role="img" aria-label="coffee cup emoji">
-            ☕️
-          </span>
-        </NavLink>
-      </Flex>
-      <Flex>
-        {/* <NavLink to="/about/">About</NavLink>
+      <Flex
+        sx={{
+          justifyContent: "space-between",
+          maxWidth: 1024,
+          m: `auto`,
+          py: 2,
+          px: 3,
+        }}
+      >
+        <SkipLink>Skip to content</SkipLink>
+        <Flex>
+          <NavLink to="/">
+            A11y Coffee{" "}
+            <span role="img" aria-label="coffee cup emoji">
+              ☕️
+            </span>
+          </NavLink>
+        </Flex>
+        <Flex>
+          {/* <NavLink to="/about/">About</NavLink>
         <NavLink href="/friday-a11y/">Friday A11Y</NavLink> */}
-        <Button
-          sx={{
-            ml: 2,
-          }}
-          onClick={cycleMode}
-        >
-          {mode === `default` ? `light` : `dark`}
-        </Button>
+          <Button
+            sx={{
+              ml: 2,
+            }}
+            onClick={cycleMode}
+          >
+            {mode === `default` ? `light` : `dark`}
+          </Button>
+        </Flex>
       </Flex>
-    </Flex>
+    </Box>
   )
 }
